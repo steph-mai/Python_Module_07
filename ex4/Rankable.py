3d335cec9c83d98fc3a274c9abf61e0ad4eb022c
Stephanie Maire <stmaire@z1r10p2.42lyon.fr>
2026-03-05 17:56:20 +0100

    ex4 dir and files

---
+++ ex4/Rankable.py
@@ -0,0 +1,31 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  Rankable.py                                       :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: stmaire <stmaire@student.42.fr>           +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/05 17:50:02 by stmaire         #+#    #+#               #
+#  Updated: 2026/03/05 17:54:28 by stmaire         ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Rankable (ABC):
+    @abstractmethod
+    def calculate_rating(self) -> int:
+        pass
+
+    @abstractmethod
+    def update_wins(self, wins: int) -> None:
+        pass
+
+    @abstractmethod
+    def update_losses(self, losses: int) -> None:
+        pass
+    
+    @abstractmethod
+    def get_rank_info(self) -> dict[str, Any]:
+        pass
